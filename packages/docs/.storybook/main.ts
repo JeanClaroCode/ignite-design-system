710a8b30be92bc34ca3f35a455cc680699ef47aa
JeanClaroCode <jeanclarodev@gmail.com>
2025-02-23 18:11:26 -0300

    A teste www

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -11,10 +11,7 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, 'package.json')))
 }
 const config: StorybookConfig = {
-  stories: [
-    '../src/**/*.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-docs',
