2763b0dcef7379f8c51ea8c0bcd47dca03d83042
JeanClaroCode <jeanclarodev@gmail.com>
2025-02-23 20:12:42 -0300

    a oshio2

--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   base: mode === 'production' ? '/ignite-design-system/' : '/', // Condicional para produção
   build: {
     outDir: 'storybook-static', // Importante: deve corresponder ao publish_dir do GitHub Pages
     assetsDir: 'assets', // Opcional: para organizar os assets
   },
-})
+}))
